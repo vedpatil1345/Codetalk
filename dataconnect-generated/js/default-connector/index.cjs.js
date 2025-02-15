a35616ba140ba22ab05e8af748a891d52690bc48
Ved Patil <vedpatil1345@gmail.com>
2025-02-15 18:10:13 +0530

    Delete dataconnect-generated/js/default-connector directory

--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,9 +0,0 @@
-const { getDataConnect, validateArgs } = require('firebase/data-connect');
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'codetalk-0.0.2',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
-
